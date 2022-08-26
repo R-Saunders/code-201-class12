818870e7e7de31dffed17498a331601e65e4c479
R-Saunders <richsaunders@protonmail.com>
2022-08-26 20:25:47 +0100

    Made a rainbow

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ var canvas = document.getElementById("myCanvas");
 
 var ctx = canvas.getContext("2d");
 
-// Rectangle
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 150, 75)
+// // Rectangle
+// ctx.fillStyle = "red";
+// ctx.fillRect(0, 0, 150, 75)
 
-// Line
-ctx.moveTo(0,0);
-ctx.lineTo(200,200);
-ctx.stroke();
+// // Line
+// ctx.moveTo(0,0);
+// ctx.lineTo(200,200);
+// ctx.stroke();
+
+// // Circle
+// ctx.beginPath();
+// ctx.arc(100, 100, 100, 0, 2 * Math.PI);
+// ctx.stroke();
+
+var grd = ctx.createLinearGradient(0, 0, 600, 600);
+grd.addColorStop(0, "red");
+grd.addColorStop(0.2, "orange");
+grd.addColorStop(0.35, "yellow");
+grd.addColorStop(0.5, "green");
+grd.addColorStop(0.65, "blue");
+grd.addColorStop(0.8, "indigo");
+grd.addColorStop(1, "violet")
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, 600, 600);
