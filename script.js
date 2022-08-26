09b66d90edd9aa7ce801e9db6fff533ba8a15600
R-Saunders <richsaunders@protonmail.com>
2022-08-26 19:46:07 +0100

    add commets

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var canvas = document.getElementById("myCanvas");
 
 var ctx = canvas.getContext("2d");
 
+// Rectangle
 ctx.fillStyle = "red";
-
 ctx.fillRect(0, 0, 150, 75)
 
+// Line
 ctx.moveTo(0,0);
 ctx.lineTo(200,200);
 ctx.stroke();
